95b168850b92bda78a902786cc80c39555e0eaba
Kitesa <kitesamerga@gmail.com>
2023-02-09 08:29:34 +0000

    Import wait_random from 0-basic_async_syntax.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Task:
-    """ task wait randomthat takes an integer
-    max_delay and returns a asyncio.Task """
+    ''' task wait randomthat takes an integer
+    max_delay and returns a asyncio.Task'''
     task = create_task(wait_random(max_delay))
     return task
